c20d1790eebf13daf949b613bbd19e0f109dbba7
Xalex79 <86166504+Xalex79@users.noreply.github.com>
2023-05-04 20:46:38 +0200

    Desplegables Trabajadores Terminados

--- src/app/js/desplegables_trabajadores.js
+++ src/app/js/desplegables_trabajadores.js
@@ -1,7 +1,16 @@
+const closeAllAcordeonItems = () => {
+    const acordeonItems = document.querySelectorAll(".acordeon-item");
+    acordeonItems.forEach(clickacordeonItem => {
+        const acordeonItemHeader = acordeonItem.querySelector(".acordeon-item-header");
+        const acordeonItemBody = acordeonItemHeader.nextElementSibling;
+        acordeonItemHeader.classList.remove("active");
+        acordeonItemBody.style.maxHeight = 0;
+    });
+};
+
 const acordeonItemHeaders = document.querySelectorAll(".acordeon-item-header");
 
 acordeonItemHeaders.forEach(acordeonItemHeader => {
-    // Agregar evento click al icono de cierre
     const closeIcon = acordeonItemHeader.querySelector(".close-icon");
     closeIcon.addEventListener("click", () => {
         const acordeonItem = acordeonItemHeader.parentNode;
@@ -23,6 +32,9 @@ acordeonItemHeaders.forEach(acordeonItemHeader => {
         else {
             acordeonItemBody.style.maxHeight = 0;
         }
-
     });
 });
+
+// Agregar evento click para cerrar todos los desplegables
+const closeAllAcordeonItemsButton = document.querySelector("#close-all-acordeon-items");
+closeAllAcordeonItemsButton.addEventListener("click", closeAllAcordeonItems);

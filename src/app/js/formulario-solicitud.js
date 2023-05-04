6f7812aacacbe311622be59984c4483e8537bf09
JuanDiaz <juanben10xlr99@gmail.com>
2023-05-04 21:56:52 +0200

    Formularios-Funcionando

--- src/app/js/formulario-solicitud.js
+++ src/app/js/formulario-solicitud.js
@@ -2,8 +2,15 @@
 document
     .querySelector("form")
     .addEventListener("submit", enviarFormulario);
+
+
+//Guardamos los id del checkbox de aceptar terminos y el boton de enviar el formulario
 const aceptarTerminos = document.getElementById("aceptar-terminos")
 const botonSolicitud = document.getElementById("boton-enviar-formulario-solicitud")
+
+
+//Añadimos un evento click al checkbox de aceptar terminos
+//cuando esté marcado la clase activo del boton se pondrá cuando no, se quitará y se pondrá la clase inactivo
 aceptarTerminos.addEventListener("click", function (){
     if(aceptarTerminos.checked){
         botonSolicitud.classList.remove("boton-enviar-formulario-solicitud-desactivado")
@@ -14,6 +21,7 @@ aceptarTerminos.addEventListener("click", function (){
         botonSolicitud.classList.remove("boton-enviar-formulario-solicitud-activo")
     }
 })
+
 
 //Funcion que se ejecuta cuando el submit del formulario de solicitud se dispara, comprueba si cada campo del formulario
 //es válido, y si es válido, muestra un popup de correcto
@@ -33,6 +41,7 @@ async function enviarFormulario(event) {
         alert('El formulario se ha enviado correctamente');
         //Vaciamos el formulario
         document.querySelector("form").reset();
+        //Se vuelve a la landing page
         location.href = "./index.html";
     }
 }

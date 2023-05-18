97c19e25a9ab457a0416d9799322c4104251a428
Xalex79 <86166504+Xalex79@users.noreply.github.com>
2023-05-19 00:30:15 +0200

    Popup trabajadores funcional (CON IDS)

--- src/app/js/desplegables_trabajadores.js
+++ src/app/js/desplegables_trabajadores.js
@@ -1,58 +1,59 @@
-// Función para cerrar todos los elementos del acordeón
-const closeAllAcordeonItems = () => {
-    // Obtén todos los elementos del acordeón
-    const acordeonItems = document.querySelectorAll(".acordeon-item");
+const open = document.getElementById('close-icon');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+const confirm = document.getElementById('confirm');
 
-    // Itera sobre cada elemento del acordeón
-    acordeonItems.forEach(acordeonItem => {
+const closeAllAcordeonItems = () => {
+    const acordeonItems = document.querySelectorAll(".acordeon-item");
+    acordeonItems.forEach(clickacordeonItem => {
         const acordeonItemHeader = acordeonItem.querySelector(".acordeon-item-header");
         const acordeonItemBody = acordeonItemHeader.nextElementSibling;
-
-        // Remueve la clase "active" del encabezado del elemento del acordeón
         acordeonItemHeader.classList.remove("active");
-
-        // Establece la altura máxima del cuerpo del elemento del acordeón a 0
         acordeonItemBody.style.maxHeight = 0;
     });
 };
 
-// Obtén todos los encabezados de los elementos del acordeón
 const acordeonItemHeaders = document.querySelectorAll(".acordeon-item-header");
 
-// Itera sobre cada encabezado del elemento del acordeón
 acordeonItemHeaders.forEach(acordeonItemHeader => {
     const closeIcon = acordeonItemHeader.querySelector(".close-icon");
-
-    // Agrega un evento de clic al icono de cierre para eliminar el elemento del acordeón
     closeIcon.addEventListener("click", () => {
         const acordeonItem = acordeonItemHeader.parentNode;
-        acordeonItem.remove();
+        confirm.addEventListener('click', () => {
+            console.log("clicado confirm");
+            acordeonItem.remove();
+            modal_container.classList.remove('show');
+        });
     });
 
-    // Agrega un evento de clic al encabezado del elemento del acordeón
     acordeonItemHeader.addEventListener("click", event => {
         const currentlyActiveAcordeonItemHeader = document.querySelector(".acordeon-item-header.active");
-
-        // Si hay un elemento del acordeón activo diferente al actual, ciérralo
-        if (currentlyActiveAcordeonItemHeader && currentlyActiveAcordeonItemHeader !== acordeonItemHeader) {
+        if(currentlyActiveAcordeonItemHeader && currentlyActiveAcordeonItemHeader!==acordeonItemHeader) {
             currentlyActiveAcordeonItemHeader.classList.toggle("active");
             currentlyActiveAcordeonItemHeader.nextElementSibling.style.maxHeight = 0;
         }
 
-        // Cambia la clase "active" del encabezado del elemento del acordeón actual
         acordeonItemHeader.classList.toggle("active");
         const acordeonItemBody = acordeonItemHeader.nextElementSibling;
-
-        // Si el encabezado del elemento del acordeón está activo, establece la altura máxima del cuerpo del elemento del acordeón a su altura real
-        if (acordeonItemHeader.classList.contains("active")) {
+        if(acordeonItemHeader.classList.contains("active")) {
             acordeonItemBody.style.maxHeight = acordeonItemBody.scrollHeight + "px";
-        } else {
-            // Si no está activo, establece la altura máxima del cuerpo del elemento del acordeón a 0
+        }
+        else {
             acordeonItemBody.style.maxHeight = 0;
         }
     });
+
+});
+open.addEventListener('click', () => {
+    modal_container.classList.add('show');
 });
 
-// Agregar evento clic para cerrar todos los elementos del acordeón
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
+
+
+
+// Agregar evento click para cerrar todos los desplegables
 const closeAllAcordeonItemsButton = document.querySelector("#close-all-acordeon-items");
 closeAllAcordeonItemsButton.addEventListener("click", closeAllAcordeonItems);

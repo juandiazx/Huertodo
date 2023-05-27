7e19b1387717b0c19f1478bfbba2908941c1c049
JuanDiaz <juanben10xlr99@gmail.com>
2023-05-27 21:43:54 +0200

    Iniciando-Metodo-API-enviarCorreoRestablecerContrasenya

---
+++ src/app/js/logica-Fake/enviarCorreoRestablecerContrasenya.js
@@ -0,0 +1,22 @@
+document.getElementById("login-form").addEventListener('submit', enviarCorreoRestablecerContrasenya);
+
+/**
+ * Se ejecutará cuando se envíe el formulario.
+ * Puesto que se usa fetch, es una función asíncrona
+ * @param event Objeto con información del evento.
+ * guardaraFormularioSolicitud()
+ */
+async function enviarCorreoRestablecerContrasenya(event) {
+    //FALTA UNIR ESTO AL POPUP DE ENVIO CON EXITO Y UN MENSAJE DE ERROR DE NO ENCONTRADO EL CORREO
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const respuesta = await fetch('../api/v.1.0/contrasenya/enviarCorreoRestablecerContrasenya.php', {
+        method: 'post',
+        body: formData
+    })
+    const data = await respuesta.json();
+    // si el resultado de la petición es OK (i.e. código HTTP 200)
+    if(respuesta.ok){
+
+    }
+}
